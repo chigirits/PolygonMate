63d84e6b4a123d03a092581327bb788a90f90896
chigirits <chigirits@gmail.com>
2024-10-12 19:08:43 +0900

    Rename example package to tokyo.chigiri.polygonmate

---
+++ Packages/tokyo.chigiri.polygonmate/Editor/ExampleEditorScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+
+namespace tokyo.chigiri.polygonmate.editor
+{
+
+    public class ExampleEditorScript
+    {
+        [MenuItem("Tools/PolygonMate/Test")]
+        static void Test()
+        {
+            EditorUtility.DisplayDialog("Example Script", "Opened This Dialog", "OK");
+        }
+    }
+
+}
